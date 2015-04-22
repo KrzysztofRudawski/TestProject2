9258f0030dd221e053c16e714387672addf5ab30
KrzysztofRudawski <k.m.rudawski@gmail.com>
2015-04-22 20:53:14 +0200

    added more code

--- WindowsApplication2/WindowsApplication2/Form1.vb
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -1,10 +1,9 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        MsgBox("Button 1")
     End Sub
 
     Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
-        MsgBox("Button 2")
+        MsgBox("Button 22")
     End Sub
 End Class
